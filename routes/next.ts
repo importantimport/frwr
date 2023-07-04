fa6c076dd64c09eedb6742ddf667fa194b8662bf
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-07-04 15:47:40 +0800

    refactor(handlers): ♻️ use includes

--- routes/next.ts
+++ routes/next.ts
@@ -5,7 +5,7 @@ import file from '../static/members.json' assert { type: 'json' }
 export const handler: Handlers = {
   GET: (req) => {
     const referrer = new URL(req.url).search.slice(1) // ?? req.headers.get('Referer')
-    const index = file.members.findIndex(({ url }) => url === referrer)
+    const index = file.members.findIndex(({ url }) => url.includes(referrer))
     const next = file.members.at(index + 1)
 
     return Response.redirect(
