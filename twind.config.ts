f06cdc6993ff66b00584d3f18b3212a462cf4261
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-07-04 15:46:10 +0800

    refactor(fresh): ⏪️ revert to twind v0

--- twind.config.ts
+++ twind.config.ts
@@ -1,11 +1,3 @@
-import { Options } from '$fresh/plugins/twindv1.ts'
-import presetTailwind from 'https://esm.sh/@twind/preset-tailwind@1.1.4'
-import presetAutoprefix from 'https://esm.sh/@twind/preset-autoprefix@1.0.7'
+import { Options } from '$fresh/plugins/twind.ts'
 
-export default {
-  presets: [
-    presetTailwind(),
-    presetAutoprefix(),
-  ],
-  selfURL: import.meta.url,
-} satisfies Options
+export default { selfURL: import.meta.url } satisfies Options
